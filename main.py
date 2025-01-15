888cba3e5635bfe5635ed2a372f2e44f30dbc3fb
JISHUBISHI <agnikbishi28@gmail.com>
2025-01-15 11:55:50 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
+import os
 import streamlit as st
 from langchain.prompts import PromptTemplate
 from langchain_community.vectorstores import FAISS
-import os
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.chains.question_answering import load_qa_chain
+from langchain.schema import Document
 from dotenv import load_dotenv
-import google.generativeai as genai
-#this is gemini api key
+from google.generativeai import configure
+
 load_dotenv()
+
 api_key = os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=api_key)
 
+def configure_genai():
+    configure(api_key=api_key)
 
-embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-faiss_index = FAISS.load_local(
-    "faiss_index",
-    embedding_model ,
-    allow_dangerous_deserialization=True
-)
+configure_genai()
 
 def get_conversational_chain():
     prompt_template = """
-    use the given recipe information and provide the user answers.
-    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    You are a recipe bot. You know all types of recipes. Suggest recipes with ingredients and preparation processes.
+    If asked unrelated questions, respond with: "I don't know, I only handle recipe-related queries."
 
     Context: {context}
     Question: {question}
@@ -31,28 +28,21 @@ def get_conversational_chain():
     Only return the helpful answer below and nothing else.
     Helpful answer:
     """
-    model = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.3)
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.3)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     return chain
 
 def user_input(user_question):
-    
-    embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    new_db = FAISS.load_local("faiss_index", embedding_model, allow_dangerous_deserialization=True)
-    docs = new_db.similarity_search(user_question)
-
     chain = get_conversational_chain()
-    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+    dummy_context = [Document(page_content="This is a placeholder context for the recipe bot.")]
+    response = chain({"input_documents": dummy_context, "question": user_question}, return_only_outputs=True)
     return response["output_text"]
 
-
 st.title("Recipe Bot")
-
 st.subheader("Chat with the Recipe BOT")
 user_message = st.text_input("Type your message here:")
 
 if user_message:
     bot_response = user_input(user_message)
-    
     st.write("*Assistant:*", bot_response)
